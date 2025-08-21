c5b66f7ffcdb0a3ccd9a4563993d51d75f626ccf
MariusNikolov <139652682+MarkataaBG04@users.noreply.github.com>
2025-08-22 02:57:03 +0300

    practice

---
+++ 01.Programming Basics with C#/02.First Steps In Coding - Exercise/04.Vacation Books List/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _04.Vacation_Books_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //За лятната ваканция в списъка със задължителна литература на Жоро има определен брой книги.
+            //Понеже Жоро предпочита да играе с приятели навън, вашата задача е да му помогнете да изчисли колко часа на ден трябва да отделя, за да прочете необходимата литература.
+
+
+            //1.Брой страници в текущата книга – цяло число в интервала[1…1000]
+            //2.Страници, които прочита за 1 час – цяло число в интервала[1…1000]
+            //3.Броят на дните, за които трябва да прочете книгата – цяло число в интервала[1…1000]
+
+            //Да се отпечата на конзолата броят часове, които Жоро трябва да отделя за четене всеки ден.
+
+            int pagesCurrentBook = int.Parse(Console.ReadLine());
+            int pagesReadPerHour = int.Parse(Console.ReadLine());
+            int daysForBook = int.Parse(Console.ReadLine());
+
+            int hoursNeed = pagesCurrentBook / pagesReadPerHour;
+            hoursNeed = hoursNeed / daysForBook;
+
+            Console.WriteLine(hoursNeed);
+        }
+    }
+}
---
+++ 01.Programming Basics with C#/02.First Steps In Coding - Exercise/07.Food Delivery/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _07.Food_Delivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ресторант отваря врати и предлага няколко менюта на преференциални цени: 
+            //    •	Пилешко меню –  10.35 лв.
+            //    •	Меню с риба – 12.40 лв.
+            //    •	Вегетарианско меню  – 8.15 лв.
+            //            Напишете програма, която изчислява колко ще струва на група хора да си поръчат храна за вкъщи.
+            //            Групата ще си поръча и десерт, чиято цена е равна на 20 % от общата сметка(без доставката). 
+            //            Цената на доставка е 2.50 лв и се начислява най - накрая.
+
+
+            //От конзолата се четат 3 реда:
+            //    •	Брой пилешки менюта – цяло число в интервала[0 … 99]
+            //    •	Брой менюта с риба – цяло число в интервала[0 … 99]
+            //    •	Брой вегетариански менюта – цяло число в интервала[0 … 99]
+            //Изход
+            //Да се отпечата на конзолата един ред:  "{цена на поръчката}"
+
+            int chickenMenusCount = int.Parse(Console.ReadLine());
+            int fishMenusCount = int.Parse(Console.ReadLine());
+            int vegeterianMenusCount = int.Parse(Console.ReadLine());
+
+            double totalSum = (chickenMenusCount * 10.35) + (fishMenusCount * 12.40) + (vegeterianMenusCount * 8.15);
+
+            double desert = totalSum * 0.20;
+
+            totalSum = totalSum + desert + 2.5;
+
+            Console.WriteLine(totalSum);
+        }
+    }
+}
