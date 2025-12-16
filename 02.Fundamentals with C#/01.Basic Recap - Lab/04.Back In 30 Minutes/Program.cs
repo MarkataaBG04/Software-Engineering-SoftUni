fa18dc4d65bd880e743bd98503b5dd53d8baca87
MariusNikolov <139652682+MarkataaBG04@users.noreply.github.com>
2025-12-16 03:06:08 +0200

    fundamentals start

---
+++ 02.Fundamentals with C#/01.Basic Recap - Lab/04.Back In 30 Minutes/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _04.Back_In_30_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes >= 60)
+            {
+                minutes = minutes % 60;
+                hours++;
+            }
+            if (hours >= 24)
+            {
+                hours = 0;
+            }
+
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
