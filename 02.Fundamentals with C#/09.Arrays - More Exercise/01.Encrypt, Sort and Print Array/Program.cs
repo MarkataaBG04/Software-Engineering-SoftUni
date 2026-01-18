9f5409c13ed02dce3d4d2af73411b2fd29581e8d
MariusNikolov <139652682+MarkataaBG04@users.noreply.github.com>
2026-01-18 23:03:01 +0200

    practise

---
+++ 02.Fundamentals with C#/09.Arrays - More Exercise/01.Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _01.Encrypt__Sort_and_Print_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] arr = new string[n];
+            int[] sortedArr = new int[n];
+
+            char[] vowels = new[] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+            for (int i = 0; i < n; i++) 
+            {
+                int sum = 0;
+                arr[i] = Console.ReadLine();
+                
+
+                char[] words = arr[i].ToCharArray();
+                
+
+                for (int j = 0; j < words.Length; j++)
+                {
+                    
+                    if (words[j] == 'a' || words[j] == 'e' || words[j] == 'i' || words[j] == 'o' || words[j] == 'u' ||
+                        words[j] == 'A' || words[j] == 'E' || words[j] == 'I' || words[j] == 'O' || words[j] == 'U')
+                    {
+                        sum += (words[j] * words.Length); 
+                    }
+                    else
+                    {
+                        sum += (words[j] / words.Length);
+                    }
+                    
+                }
+                sortedArr[i] = sum;
+            }
+
+
+            Array.Sort(sortedArr);
+            foreach (int i in sortedArr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
